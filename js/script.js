926c2222abab6807790a88a9343ae792dcf2a469
lilyyee <emaillilyyee@gmail.com>
2021-08-02 14:22:52 -0700

    Fetch and Display User Data

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const overview = document.querySelector(".overview");   // This is where my profile info will appear
+const username = "lilyyee";   // This is my Github username
+
+// Fetch GitHub API data
+const gitUserInfo = async function (){
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    //console.log(data);
+    displayUserInfo(data);
+};
+
+gitUserInfo();
+
+// Display User Info
+const displayUserInfo = function (data){
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+    `;
+    overview.append(div);
+};
